9949ddecac6b4c714bd2fab045dca0c2bb885cd7
woojoo0425 <shinwoojoo1995@yahoo.co.jp>
2021-12-18 17:43:43 +0900

    add extracting companies
    add extracting companies

---
+++ indeed.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"http://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+def extract_indeed_pages():
+  resul = requests.get(URL)
+
+  soup = BeautifulSoup(resul.text, "html.parser")
+
+  pagination = soup.find("div", {"class":"pagination"})
+
+  links = pagination.find_all('a')
+  pages = []
+  for link in links[:-1]:
+    pages.append(int(link.string))
+
+  max_page = pages[-1]
+  return max_page
+
+def extract_indeed_jobs(last_pages):
+  jobs = []
+  # for page in range(last_pages):
+  result = requests.get(f"{URL}&start={0*LIMIT}")
+  soup = BeautifulSoup(result.text, "html.parser")
+  results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+  for result in results:
+    title = result.find("div", {"class": "title"}).find("a")["title"]
+    company = result.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+      company = str(company_anchor.string)
+    else:
+      company = str((company.string))
+    company = company.strip()
+  return jobs
