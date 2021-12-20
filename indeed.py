78e9c8e7ea9abe1ad4f25c36ee07e32661859a67
woojoo0425 <shinwoojoo1995@yahoo.co.jp>
2021-12-20 18:08:23 +0900

    add so.py
    add so.py

--- indeed.py
+++ indeed.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-URL = f"http://www.indeed.com/jobs?q=python&limit={LIMIT}"
+URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_pages():
   resul = requests.get(URL)
   soup = BeautifulSoup(resul.text, "html.parser")
   pagination = soup.find("div", {"class":"pagination"})
@@ -27,9 +27,9 @@ def extract_job(result):
   company = company.strip()
   location = result.find("div", {"class": "companyLocation"}).string
   job_id = result.parent["data-jk"]
-  return {'title': title, 'company': company, 'location': location, 'link': f"https://www.indeed.com/viewjob?jk={job_id}"}
+  return {'title': title, 'company': company, 'location': location, 'link': f"http://www.indeed.com/viewjob?jk={job_id} "}
 
-def extract_indeed_jobs(last_pages):
+def extract_jobs(last_pages):
   jobs = []
   for page in range(last_pages):
     result = requests.get(f"{URL}&start={page*LIMIT}")
@@ -38,4 +38,9 @@ def extract_indeed_jobs(last_pages):
     for result in results:
       job = extract_job(result)
       jobs.append(job)
+  return jobs
+
+def get_jobs():
+  last_page = get_last_pages()
+  jobs = extract_jobs(last_page)
   return jobs
