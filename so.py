d0a763dd15b9aedbdbabc7b376c1248adf75a45c
woojoo0425 <shinwoojoo1995@yahoo.co.jp>
2021-12-22 15:42:59 +0900

    add save_to_file
    add save_to_file

--- so.py
+++ so.py
@@ -1,46 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-LIMIT = 50
-URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+URL = f"https://stackoverflow.com/jobs?q=python&pg=i"
 
-def get_last_pages():
-  resul = requests.get(URL)
-  soup = BeautifulSoup(resul.text, "html.parser")
-  pagination = soup.find("div", {"class":"pagination"})
-
-  links = pagination.find_all('a')
-  pages = []
-  for link in links[0:-1]:
-    pages.append(int(link.string))
-  max_page = pages[-1]
-  return max_page
+def get_last_page():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+  last_page = pages[-2].get_text(strip=True)
+  return int(last_page)
 
 def extract_job(result):
-  title = result.select_one('.jobTitle>span').string
-  company = result.find("span", {"class": "companyName"})
-  company_anchor = company.find("a")
-  if company_anchor is not None:
-    company = str(company_anchor.string)
-  else:
-    company = str((company.string))
-  company = company.strip()
-  location = result.find("div", {"class": "companyLocation"}).string
-  job_id = result.parent["data-jk"]
-  return {'title': title, 'company': company, 'location': location, 'link': f"http://www.indeed.com/viewjob?jk={job_id} "}
+  title = result.find("h2", {"class": "mb4"}).find("a")["title"]
+  company, location = result.find("h3", {"class": "mb4"}).find_all("span", recursive=False)
+  company = company.get_text(strip=True)
+  location = location.get_text(strip=True)
+  job_id = result["data-jobid"]
+  return {'title': title, 'company': company, 'location': location, 'link': f"https://stackoverflow.com/jobs/{job_id}"}
 
-def extract_jobs(last_pages):
+
+def extract_jobs(last_page):
   jobs = []
-  for page in range(last_pages):
-    result = requests.get(f"{URL}&start={page*LIMIT}")
+  for page in range(last_page):
+    result = requests.get(f"{URL}&pg={page+1}")
     soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "slider_container"})
+    results = soup.find_all("div", {"class": "-job"})
     for result in results:
       job = extract_job(result)
       jobs.append(job)
   return jobs
 
-def get_jobs():
-  last_page = get_last_pages()
+def get_jobs(): 
+  last_page = get_last_page()
   jobs = extract_jobs(last_page)
   return jobs
